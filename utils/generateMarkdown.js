069133b2ea22286004df0e84d7e5ce95ba639385
Luke <116241757+Nyrexis@users.noreply.github.com>
2023-01-24 22:44:36 +1100

    mostly functioning license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (value === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else if (value === "Apache-2.0") {
-    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  } else if (value === "FreeBSD") {
-    return "[![License: BSD-2-Clause](https://img.shields.io/badge/License-BSD-brightgreen.svg)](https://opensource.org/licenses/BSD-2-Clause)";
-  } else if (value === "GNU GPL") {
-    return "[![License: GNU GPL](https://img.shields.io/badge/License-GPL-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-  } else if (value === "MPL-2.0") {
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  let input = license.license;
+  
+  if (input === "MIT") {
+    return "https://img.shields.io/badge/License-MIT-yellow.svg";
+  } else if (input === "Apache-2.0") {
+    return "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+  } else if (input === "FreeBSD") {
+    return "https://img.shields.io/badge/License-BSD-brightgreen.svg";
+  } else if (input === "GNU GPL") {
+    return "https://img.shields.io/badge/License-GPL-blue.svg";
+  } else if (input === "MPL-2.0") {
+    return "https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg";
+  } else if (input === "EPL-2.0") {
+    return "https://img.shields.io/badge/License-EPL%202.0-green.svg";
   } else {
     return ""
   }
@@ -18,16 +22,19 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (value === "MIT") {
+  let input = license.license;
+  if (input === "MIT") {
     return "https://opensource.org/licenses/MIT";
-  } else if (value === "Apache-2.0") {
+  } else if (input === "Apache-2.0") {
     return "https://opensource.org/licenses/Apache-2.0";
-  } else if (value === "FreeBSD") {
-    return "(https://opensource.org/licenses/BSD-2-Clause)";
-  } else if (value === "GNU GPL") {
-    return "https://www.gnu.org/licenses/gpl-3.0";
-  } else if (value === "MPL-2.0") {
+  } else if (input === "FreeBSD") {
+    return "https://opensource.org/licenses/BSD-2-Clause";
+  } else if (input === "GNU GPL") {
+    return "https://opensource.org/licenses/GPL-3.0";
+  } else if (input === "MPL-2.0") {
     return "https://opensource.org/licenses/MPL-2.0";
+  } else if (input === "EPL-2.0") {
+    return "https://opensource.org/licenses/EPL-2.0";
   } else {
     return ""
   }
@@ -35,8 +42,11 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  let badge = renderLicenseBadge(license)
+  let link = renderLicenseLink(license)
+  return `![License: ${license}] !(${badge}) , (${link})`
+}
 
 function renderTableOfContents(data){
   console.log(data)
@@ -61,9 +71,10 @@ function renderTableOfContents(data){
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   let contents = renderTableOfContents(data)
-  let licenseBadge = renderLicenseBadge()
+  let licenseSpot = renderLicenseSection(data)
+  let licenseBadge = renderLicenseBadge(data)
   let str = 
-  ` # ${data.title}
+  ` # ${data.title} ![${data.license}](${licenseBadge})
   
   ## Description
   ${data.description} 
@@ -86,14 +97,11 @@ function generateMarkdown(data) {
   
   ## License
   
- 
-  
-  ## Badges
-  
+  ${licenseSpot}
   
   ## Contact
   
-  Github: ${data.git}, Email: ${data.email}
+  Github: [${data.git}](https://github.com/${data.git}) , Email: ${data.email}
   
   ## Tests
   ${data.tests}
